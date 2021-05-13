765271b9a2a8c4cf050592c6c8a44fdbd2e29154
rendoruiz <rendo.ruiz@outlook.com>
2021-05-13 15:12:33 -0600

    lesson 29

--- flutter_guide/lib/main.dart
+++ flutter_guide/lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('This is a title'),
+      ),
+      body: Text('This is the content'),
+    ));
   }
 }
