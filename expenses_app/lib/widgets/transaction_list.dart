4849a6029577e6c120b6c192155d7b5cf9d0bd41
rendoruiz <rendo.ruiz@outlook.com>
2021-05-16 12:58:57 -0600

    l126: custom layouts based on device orientation

--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -12,24 +12,26 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: <Widget>[
-              Text(
-                'No transactions added yet.!',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Container(
-                height: 200,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
+        ? LayoutBuilder(builder: (ctx, constrains) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet.!',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-              )
-            ],
-          )
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: constrains.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            );
+          })
         : ListView.builder(
             itemBuilder: (ctx, index) {
               return Card(
