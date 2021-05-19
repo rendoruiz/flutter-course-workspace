927c1f07bc6a8e0fe45e7d8e35cf35bac687340f
rendoruiz <rendo.ruiz@outlook.com>
2021-05-19 12:18:12 -0600

    l163: named pages

--- meals_app/lib/category_item.dart
+++ meals_app/lib/category_item.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 import './category_meals_page.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
   void selectCagetory(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryMealsPage();
+          return CategoryMealsPage(id, title);
         },
       ),
     );
--- meals_app/lib/category_meals_page.dart
+++ meals_app/lib/category_meals_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsPage extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsPage(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('The Recipes For The Category!'),
