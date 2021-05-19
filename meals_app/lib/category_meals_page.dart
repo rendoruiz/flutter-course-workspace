6795e6cacfd00cb0f248233ea6ef2f1ce1012baf
rendoruiz <rendo.ruiz@outlook.com>
2021-05-19 13:05:32 -0600

    167

--- meals_app/lib/category_meals_page.dart
+++ meals_app/lib/category_meals_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealsPage extends StatelessWidget {
   static const routeName = '/category-meals';
   // final String categoryId;
@@ -11,13 +13,21 @@ class CategoryMealsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
-    final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'] ?? '';
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: ListView.builder(itemBuilder: (ctx, index) {}),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }
