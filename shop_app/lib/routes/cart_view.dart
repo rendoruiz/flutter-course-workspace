32df91ac8b2a57da3d07d25e20698fe1186d8033
rendoruiz <rendo.ruiz@outlook.com>
2021-05-19 23:16:29 -0600

    l209: adding orders

--- shop_app/lib/routes/cart_view.dart
+++ shop_app/lib/routes/cart_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart' show Cart;
+import 'package:shop_app/providers/orders.dart';
 import '../widgets/cart_item.dart';
 
 class CartView extends StatelessWidget {
@@ -38,7 +39,13 @@ class CartView extends StatelessWidget {
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
                     child: Text('ORDER NOW'),
                     textColor: Theme.of(context).primaryColor,
                   )
