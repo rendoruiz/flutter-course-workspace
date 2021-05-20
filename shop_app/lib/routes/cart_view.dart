10194b765a995bfc81c89fd53f8a9513cee56c34
rendoruiz <rendo.ruiz@outlook.com>
2021-05-19 22:55:20 -0600

    l205: cart items
    used .values.toList() to access an item on a map object

--- shop_app/lib/routes/cart_view.dart
+++ shop_app/lib/routes/cart_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartView extends StatelessWidget {
   static const routeName = '/cart';
@@ -42,6 +43,20 @@ class CartView extends StatelessWidget {
                     textColor: Theme.of(context).primaryColor,
                   )
                 ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) => CartItem(
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title,
               ),
             ),
           ),
