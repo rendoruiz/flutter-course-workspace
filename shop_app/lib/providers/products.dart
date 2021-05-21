6f2c42f50a773fcc1a021582d75c6f52a09dad1b
rendoruiz <rendo.ruiz@outlook.com>
2021-05-20 21:02:02 -0600

    l33: delete product

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -81,4 +81,9 @@ class Products with ChangeNotifier {
     _items[prodIndex] = newProduct;
     notifyListeners();
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((prod) => prod.id == id);
+    notifyListeners();
+  }
 }
--- shop_app/lib/widgets/user_product_item.dart
+++ shop_app/lib/widgets/user_product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/routes/edit_product_view.dart';
 
 class UserProductItem extends StatelessWidget {
@@ -31,7 +33,9 @@ class UserProductItem extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
             ),
           ],
         ),
