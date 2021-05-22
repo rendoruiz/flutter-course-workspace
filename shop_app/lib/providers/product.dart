52b673df6b17efe33efd05998ec27d9d9203b140
rendoruiz <rendo.ruiz@outlook.com>
2021-05-21 22:04:56 -0600

    l258: favourite status update

--- shop_app/lib/providers/product.dart
+++ shop_app/lib/providers/product.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +19,32 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavouriteStatus() {
+  void _setFavouriteValue(bool newValue) {
+    isFavourite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavouriteStatus() async {
+    final oldStatus = isFavourite;
+
     isFavourite = !isFavourite;
     notifyListeners();
+    final url = Uri.https(
+        'flutter-test-63fa4-default-rtdb.firebaseio.com', '/products/$id.json');
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode(
+          {
+            'isFavourite': isFavourite,
+          },
+        ),
+      );
+      if (response.statusCode >= 400) {
+        _setFavouriteValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavouriteValue(oldStatus);
+    }
   }
 }
