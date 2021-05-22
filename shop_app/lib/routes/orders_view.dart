61e084c2f8ca5f797dffdf5e007eb24d775a8b52
rendoruiz <rendo.ruiz@outlook.com>
2021-05-21 22:36:35 -0600

    l260: fetching orders

--- shop_app/lib/routes/orders_view.dart
+++ shop_app/lib/routes/orders_view.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders.dart' show Orders;
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
-class OrdersView extends StatelessWidget {
+class OrdersView extends StatefulWidget {
   static const routeName = '/orders';
+
+  @override
+  _OrdersViewState createState() => _OrdersViewState();
+}
+
+class _OrdersViewState extends State<OrdersView> {
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +37,14 @@ class OrdersView extends StatelessWidget {
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+            ),
     );
   }
 }
