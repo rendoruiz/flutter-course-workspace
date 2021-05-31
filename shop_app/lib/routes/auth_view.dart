fc7878b0d5e5711bc610150b0e384f04c50097e4
rendoruiz <rendo.ruiz@outlook.com>
2021-05-31 16:30:22 -0600

    l271: flutter firebase auth login

--- shop_app/lib/routes/auth_view.dart
+++ shop_app/lib/routes/auth_view.dart
@@ -116,10 +116,16 @@ class _AuthCardState extends State<AuthCard> {
     });
     if (_authMode == AuthMode.Login) {
       // Log user in
+      await Provider.of<Auth>(context, listen: false).login(
+        _authData['email'],
+        _authData['password'],
+      );
     } else {
       // Sign user up
-      await Provider.of<Auth>(context, listen: false)
-          .signup(_authData['email'], _authData['password']);
+      await Provider.of<Auth>(context, listen: false).signup(
+        _authData['email'],
+        _authData['password'],
+      );
     }
     setState(() {
       _isLoading = false;
